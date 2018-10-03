4dcc1b79a8602f7192a23b0627151c348bf1c920
Emily Pitts <emilypitts@emilys-mbp-2.gateway.sonic.net>
2018-10-03 14:44:45 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+This is a simple exercise for practicing adding, removing, and manipulating elements in the DOM. There are four tasks to complete, as you will see in index.html. Check out domManipulation.js and look for the TODO comments for where you need to add code! You shouldn't need to edit HTML or CSS, but as always, you're free to make changes. I recommend taking a look at the CSS to see how the layout of the page is accomplished.
+
+1. (5 points) Apply a color to all boxes by click one one of the colors in task 1.
+2. (5 points) Add a new gray box to the end of the boxes display.
+3. (5 points) Add ability to 'select' boxes. Selected boxes should have a border.
+4. (5 points) Clicking 'Remove Boxes' should remove all selected boxes from the DOM.
+5. (Extra Credit: 2 points) Come up with and add your own box manipulation task!
+
+HINT: You may need to add multiple classes to an element. You can do this by setting the class attribute to a string with class names separated by spaces. For example: `element.className="box box-selected"` would apply both the box and box-selected classes.
---
+++ js/domManipulation.js
@@ -0,0 +1,55 @@
+
+function changeBoxColors(color) {
+  console.log('Selected color: ' + color);
+  // TODO: Look at styles.css and choose a class
+  // to apply to all of the box elements in order
+  // to change their background color
+  // possible values of color are: 'red', 'blue', 'yellow'
+  // (consider using a switch statement!)
+}
+
+function addBox() {
+  console.log('Adding a new box');
+  // TODO: Add a new div with class="box" to
+  // the boxes section of the dom tree
+
+  var newBoxElement = null; // replace with your code
+  // This line is needed to make sure that new boxes
+  // handle clicks. Make sure thatnewBoxElement refers
+  // to the DOM node containing your new Div.
+  newBoxElement.addEventListener('click', handleBoxClick);
+}
+
+var selectedBoxes = [];
+function removeSelectedBoxes() {
+  console.log('Removing selected boxes');
+  // TODO: look at the selectedBoxes array and remove each of those
+  // from their parent in the DOM tree (their parent is the div with id="boxes").
+  // Think about what happens to the selectedBoxes array when you're done!
+}
+
+/* Event Handlers */
+function handleBoxClick(event) {
+  var boxElement = event.target;
+  console.log('Selecting box: ', boxElement);
+  // TODO: add or remove the box from the array of selectedBoxes
+  // TODO: looke at styles.css and choose a class (or multiple classes)
+  // to apply in order to add a border around selected boxes or
+  // remove a border from deselected boxes
+}
+
+function handleColorSelect(event) {
+  changeBoxColors(event.target.id);
+}
+
+function attachListeners() {
+  document.querySelectorAll('.box').forEach(function(box){
+    box.addEventListener('click', handleBoxClick);
+  });
+  document.querySelectorAll('.color-selector').forEach(function(color){
+    color.addEventListener('click', handleColorSelect);
+  });
+  document.querySelector('#addButton').addEventListener('click', addBox);
+  document.querySelector('#removeButton').addEventListener('click', removeSelectedBoxes);
+}
+document.addEventListener("DOMContentLoaded", attachListeners);
